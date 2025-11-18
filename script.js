69fe1fb649e58a3781485751729257de65330aa5
João Pedro Gianezini <84107419+checkmator@users.noreply.github.com>
2025-11-18 09:00:26 -0400

    Add script to validate hostname and clear content

---
+++ script.js
@@ -0,0 +1,12 @@
+<script>
+	(function(){
+		var _0x12a7="c2V1LWRvbWluaW8uY29t"; 
+		function _0x4bc1(_0x1){return decodeURIComponent(escape(window.atob(_0x1)));}
+		var _0x9fd0 = window.location.hostname.toLowerCase().replace("www.","");
+		var _0x2ce7 = _0x4bc1(_0x12a7);
+		if(_0x9fd0 !== _0x2ce7){
+			document.documentElement.innerHTML = "";
+			setTimeout(function(){document.write("");}, 50);
+		}
+	})();
+</script>
